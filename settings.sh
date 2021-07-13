390a0ff1aba59e4c6db48356a39ae628949cba80
konsole257 <87348050+konsole257@users.noreply.github.com>
2021-07-13 11:01:47 +0900

    Add files via upload

---
+++ settings.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+echo ""
+echo -e '\E[00;31m'"\033[1m/************************************\033[0m"
+echo ""
+echo -e '\E[00;31m'"\033[1m      npm & gulp setting\033[0m"
+echo ""
+echo -e '\E[00;31m'"\033[1m************************************/\033[0m"
+echo ""
+npm init -y
+npm i gulp --save-dev
+npm i gulp-webserver --save-dev
+npm i del --save-dev
+npm i gulp-concat --save-dev
+npm i gulp-imagemin --save-dev
+npm i gulp-watch --save-dev
+npm i gulp-sass --save-dev
+npm i gulp-file-include --save-dev
+npm i gulp-replace --save-dev
+
+echo ""
+echo -e '\E[00;31m'"\033[1m/************************************\033[0m"
+echo ""
+echo -e '\E[00;31m'"\033[1m      WorkSpace\033[0m"
+echo ""
+echo -e '\E[00;31m'"\033[1m************************************/\033[0m"
+echo ""
+path=$0
+dir=${path%_*}
+
+cd ${dir}
+
+mkdir public
+mkdir public/src
+
+echo ""
+echo -e '\E[00;31m'"\033[1m/************************************\033[0m"
+echo ""
+echo -e '\E[00;31m'"\033[1m      완료\033[0m"
+echo ""
+echo -e '\E[00;31m'"\033[1m************************************/\033[0m"
+echo ""
+
+exit 0
